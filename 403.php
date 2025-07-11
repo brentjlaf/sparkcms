63e0e8a34d0039337e2886badad22cf75d443687
brentjlaf <brentjlaf@hotmail.com>
2025-07-11 08:25:31 -0600

    Add files via upload

---
+++ 403.php
@@ -0,0 +1,19 @@
+<?php
+http_response_code(403);
+$settingsFile = __DIR__ . '/CMS/data/settings.json';
+$menusFile = __DIR__ . '/CMS/data/menus.json';
+$settings = file_exists($settingsFile) ? json_decode(file_get_contents($settingsFile), true) : [];
+$menus = file_exists($menusFile) ? json_decode(file_get_contents($menusFile), true) : [];
+$scriptBase = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+if (substr($scriptBase, -4) === '/CMS') {
+    $scriptBase = substr($scriptBase, 0, -4);
+}
+$scriptBase = rtrim($scriptBase, '/');
+$page = ['title' => 'Restricted', 'content' => '<h1>Restricted</h1>'];
+$themeBase = $scriptBase . '/theme';
+$templateFile = __DIR__ . '/theme/templates/pages/errors/403.php';
+ob_start();
+include $templateFile;
+$html = ob_get_clean();
+$html = preg_replace('/<mwPageArea[^>]*><\\/mwPageArea>/', $page['content'], $html);
+echo $html;
---
+++ router.php
@@ -0,0 +1,7 @@
+<?php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $path;
+if ($path !== '/' && file_exists($file)) {
+    return false; // serve the requested resource as-is
+}
+require __DIR__ . '/index.php';
