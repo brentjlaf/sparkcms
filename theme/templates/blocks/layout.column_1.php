5b57bababc22d147d86259e007bce004489821f5
brentjlaf <brentjlaf@hotmail.com>
2025-07-11 15:56:07 -0600

    Add column layout subtemplates

---
+++ theme/templates/blocks/layout.column_1.php
@@ -0,0 +1,6 @@
+<!-- File: layout.column_1.php -->
+<!-- Template: layout.column_1 -->
+<div class="row drop-area" data-tpl-tooltip="1 Column">
+    <div class="col"><div class="drop-area"></div></div>
+</div>
+
---
+++ theme/templates/blocks/layout.column_3.php
@@ -0,0 +1,8 @@
+<!-- File: layout.column_3.php -->
+<!-- Template: layout.column_3 -->
+<div class="row drop-area" data-tpl-tooltip="3 Columns">
+    <div class="col"><div class="drop-area"></div></div>
+    <div class="col"><div class="drop-area"></div></div>
+    <div class="col"><div class="drop-area"></div></div>
+</div>
+
