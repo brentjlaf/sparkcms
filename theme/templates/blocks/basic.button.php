ecc64863b2dd243826d57da8c9802c4e6b7c6808
brentjlaf <brentjlaf@hotmail.com>
2025-07-12 02:28:57 -0600

    Add alignment and new window options to button block

--- theme/templates/blocks/basic.button.php
+++ theme/templates/blocks/basic.button.php
@@ -9,5 +9,19 @@
         <dt>Link</dt>
         <dd><input type="text" name="custom_link" value="#"></dd>
     </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Open in New Window</dt>
+        <dd><label><input type="checkbox" name="custom_new_window" value=' target="_blank"'> New window</label></dd>
+    </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Alignment</dt>
+        <dd>
+            <label><input type="checkbox" name="custom_align_left" value=" _text-left"> Left</label>
+            <label><input type="checkbox" name="custom_align_center" value=" _text-center"> Center</label>
+            <label><input type="checkbox" name="custom_align_right" value=" _text-right"> Right</label>
+        </dd>
+    </dl>
 </templateSetting>
-<a href="{custom_link}" class="btn btn-primary" data-tpl-tooltip="Button" data-editable>{custom_text}</a>
+<div class="{custom_align_left}{custom_align_center}{custom_align_right}">
+    <a href="{custom_link}" class="btn btn-primary"{custom_new_window} data-tpl-tooltip="Button" data-editable>{custom_text}</a>
+</div>
