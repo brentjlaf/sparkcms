5164dee301ae5d365c13f3f7994b81c3fde8e1d5
brentjlaf <brentjlaf@hotmail.com>
2025-07-12 14:05:54 -0600

    Add CTA banner block

---
+++ theme/templates/blocks/basic.cta-banner.php
@@ -0,0 +1,31 @@
+<!-- File: basic.cta-banner.php -->
+<!-- Template: basic.cta-banner -->
+<templateSetting caption="CTA Banner Settings" order="1">
+    <dl class="sparkDialog _tpl-box">
+        <dt>Heading</dt>
+        <dd><input type="text" name="custom_heading" value="Join Us Today"></dd>
+    </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Subtext</dt>
+        <dd><textarea name="custom_subtext">Start your journey with us.</textarea></dd>
+    </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Button Text</dt>
+        <dd><input type="text" name="custom_btn_text" value="Get Started"></dd>
+    </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Button Link</dt>
+        <dd><input type="text" name="custom_btn_link" value="#"></dd>
+    </dl>
+    <dl class="sparkDialog _tpl-box">
+        <dt>Open in New Window</dt>
+        <dd><label><input type="checkbox" name="custom_btn_new" value=' target="_blank"'> New window</label></dd>
+    </dl>
+</templateSetting>
+<div class="cta-banner" data-tpl-tooltip="CTA Banner">
+    <div class="container cta-banner-content">
+        <h2 data-editable>{custom_heading}</h2>
+        <p data-editable>{custom_subtext}</p>
+        <a href="{custom_btn_link}" class="btn btn-primary"{custom_btn_new} data-editable>{custom_btn_text}</a>
+    </div>
+</div>
