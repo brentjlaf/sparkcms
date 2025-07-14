0f500bbdf70c7e5fc4f4080813107f94a8ddb61a
brentjlaf <brentjlaf@hotmail.com>
2025-07-13 20:27:43 -0600

    Remove redundant page templates

--- theme/templates/pages/contact-us.php
+++
@@ -1,4 +0,0 @@
-<?php
-// File: contact-us.php
-// Template: contact-us
-require __DIR__ . '/page.php';
--- theme/templates/pages/services.php
+++
@@ -1,4 +0,0 @@
-<?php
-// File: services.php
-// Template: services
-require __DIR__ . '/page.php';
