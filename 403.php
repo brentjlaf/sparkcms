631181e0b03c5677918756b3d880bd9247b3373d
brentjlaf <brentjlaf@hotmail.com>
2025-07-15 08:21:19 -0600

    Improve error pages

--- 403.php
+++ 403.php
@@ -10,7 +10,15 @@ if (substr($scriptBase, -4) === '/CMS') {
     $scriptBase = substr($scriptBase, 0, -4);
 }
 $scriptBase = rtrim($scriptBase, '/');
-$page = ['title' => 'Restricted', 'content' => '<h1>Restricted</h1>'];
+$page = [
+    'title' => 'Restricted',
+    'content' =>
+        '<h1>Restricted</h1>' .
+        '<p>You do not have permission to view this page.</p>' .
+        '<p><a href="' . htmlspecialchars($scriptBase) . '/">Return to homepage</a>' .
+        ' or <a href="' . htmlspecialchars($scriptBase) . '/CMS/login.php">log in</a> ' .
+        'with an account that has access.</p>'
+];
 $themeBase = $scriptBase . '/theme';
 $templateFile = __DIR__ . '/theme/templates/pages/errors/403.php';
 ob_start();
