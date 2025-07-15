0e462cce94fca68a9888187c3a16326db342edf8
brentjlaf <brentjlaf@hotmail.com>
2025-07-15 12:15:57 -0600

    Use glob for listing blocks

--- liveed/list-blocks.php
+++ liveed/list-blocks.php
@@ -4,12 +4,13 @@ require_once __DIR__ . '/../CMS/includes/auth.php';
 require_login();
 
 $blocksDir = __DIR__ . '/../theme/templates/blocks';
+
 $blocks = [];
 if (is_dir($blocksDir)) {
-    foreach (scandir($blocksDir) as $f) {
-        if (substr($f, -4) === '.php') {
-            $blocks[] = $f;
-        }
+    $paths = glob($blocksDir . '/*.php');
+    if ($paths !== false) {
+        $blocks = array_map('basename', $paths);
+        sort($blocks, SORT_STRING);
     }
 }
 header('Content-Type: application/json');
