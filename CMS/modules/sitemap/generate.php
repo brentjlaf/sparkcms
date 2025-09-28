b016f50a1e5b30ac617cdf107b4398cac6c0e886
brentjlaf <brentjlaf@hotmail.com>
2025-09-28 00:30:55 -0600

    Handle sitemap generation without DOM extension

--- CMS/modules/sitemap/generate.php
+++ CMS/modules/sitemap/generate.php
@@ -27,23 +27,11 @@ try {
     $scriptBase = rtrim($scriptBase, '/');
     $baseUrl = $scheme . '://' . $host . $scriptBase;
 
-    $dom = new DOMDocument('1.0', 'UTF-8');
-    $urlset = $dom->createElement('urlset');
-    $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-
     $entries = [];
     foreach ($published as $page) {
         $slug = ltrim((string)($page['slug'] ?? ''), '/');
         $lastModified = isset($page['last_modified']) ? (int)$page['last_modified'] : time();
-
-        $url = $dom->createElement('url');
-        $loc = $dom->createElement('loc', $baseUrl . '/' . $slug);
         $lastmodDate = date('Y-m-d', $lastModified);
-        $lastmod = $dom->createElement('lastmod', $lastmodDate);
-
-        $url->appendChild($loc);
-        $url->appendChild($lastmod);
-        $urlset->appendChild($url);
 
         $entries[] = [
             'title' => (string)($page['title'] ?? ''),
@@ -54,11 +42,51 @@ try {
         ];
     }
 
-    $dom->appendChild($urlset);
-    $dom->formatOutput = true;
     $sitemapPath = __DIR__ . '/../../../sitemap.xml';
-    if ($dom->save($sitemapPath) === false) {
-        throw new RuntimeException('Unable to write sitemap file.');
+    $domAvailable = class_exists('DOMDocument');
+
+    if ($domAvailable) {
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $urlset = $dom->createElement('urlset');
+        $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+        foreach ($entries as $entry) {
+            $url = $dom->createElement('url');
+            $loc = $dom->createElement('loc', $entry['url']);
+            $lastmod = $dom->createElement('lastmod', $entry['lastmod']);
+
+            $url->appendChild($loc);
+            $url->appendChild($lastmod);
+            $urlset->appendChild($url);
+        }
+
+        $dom->appendChild($urlset);
+        $dom->formatOutput = true;
+
+        if ($dom->save($sitemapPath) === false) {
+            throw new RuntimeException('Unable to write sitemap file.');
+        }
+    } else {
+        $lines = [
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+        ];
+
+        foreach ($entries as $entry) {
+            $loc = htmlspecialchars($entry['url'], ENT_XML1);
+            $lastmod = htmlspecialchars($entry['lastmod'], ENT_XML1);
+            $lines[] = '  <url>';
+            $lines[] = '    <loc>' . $loc . '</loc>';
+            $lines[] = '    <lastmod>' . $lastmod . '</lastmod>';
+            $lines[] = '  </url>';
+        }
+
+        $lines[] = '</urlset>';
+        $xml = implode("\n", $lines);
+
+        if (file_put_contents($sitemapPath, $xml) === false) {
+            throw new RuntimeException('Unable to write sitemap file.');
+        }
     }
 
     $generatedAt = filemtime($sitemapPath) ?: time();
@@ -71,6 +99,7 @@ try {
         'generatedAtFormatted' => date('F j, Y g:i a', $generatedAt),
         'entries' => $entries,
         'sitemapUrl' => $baseUrl . '/sitemap.xml',
+        'generator' => $domAvailable ? 'dom' : 'simple',
     ]);
 } catch (Throwable $exception) {
     http_response_code(500);
