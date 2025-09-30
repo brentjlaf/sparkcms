127710394e3c97d3e066b0e71375b9cbacefd9d7
brentjlaf <brentjlaf@hotmail.com>
2025-09-30 15:33:43 -0600

    Pin homepage at top of pages list

--- CMS/modules/pages/list_pages.php
+++ CMS/modules/pages/list_pages.php
@@ -2,13 +2,29 @@
 // File: list_pages.php
 require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/data.php';
+require_once __DIR__ . '/../../includes/settings.php';
 require_login();
 
 $pagesFile = __DIR__ . '/../../data/pages.json';
 $pages = read_json_file($pagesFile);
+$settings = get_site_settings();
+$homepageSlug = $settings['homepage'] ?? 'home';
+$homePage = null;
 $result = [];
+
 foreach ($pages as $p) {
-    $result[] = ['id' => $p['id'], 'title' => $p['title'], 'slug' => $p['slug']];
+    $pageData = ['id' => $p['id'], 'title' => $p['title'], 'slug' => $p['slug']];
+
+    if ($p['slug'] === $homepageSlug) {
+        $homePage = $pageData;
+        continue;
+    }
+
+    $result[] = $pageData;
+}
+
+if ($homePage !== null) {
+    array_unshift($result, $homePage);
 }
 
 echo json_encode($result);
