80eac051803e12d2033c471f9d2c48568b6cf6dc
brentjlaf <brentjlaf@hotmail.com>
2025-10-03 16:29:00 -0600

    Adjust menu classes for dropdown items

--- theme/templates/pages/page.php
+++ theme/templates/pages/page.php
@@ -21,16 +21,42 @@ $bodyAttributes = 'class="d-flex flex-column min-vh-100"';
 function renderMenu($items, $isDropdown = false){
     foreach ($items as $it) {
         $hasChildren = !empty($it['children']);
+
+        $liClasses = $isDropdown ? ['dropdown-item'] : ['nav-item'];
+        $linkClasses = $isDropdown ? ['dropdown-item'] : ['nav-link'];
+        $linkAttributes = [];
+
         if ($hasChildren) {
-            echo '<li class="nav-item dropdown">';
-            echo '<a class="nav-link dropdown-toggle" href="'.htmlspecialchars($it['link']).'"'.(!empty($it['new_tab']) ? ' target="_blank"' : '').' role="button" data-bs-toggle="dropdown" aria-expanded="false">'.htmlspecialchars($it['label']).'</a>';
+            if ($isDropdown) {
+                $liClasses[] = 'dropdown-submenu';
+                $liClasses[] = 'dropend';
+                $linkClasses[] = 'dropdown-toggle';
+            } else {
+                $liClasses[] = 'dropdown';
+                $linkClasses[] = 'dropdown-toggle';
+            }
+            $linkAttributes[] = 'role="button"';
+            $linkAttributes[] = 'data-bs-toggle="dropdown"';
+            $linkAttributes[] = 'aria-expanded="false"';
+        }
+
+        if (!empty($it['new_tab'])) {
+            $linkAttributes[] = 'target="_blank"';
+        }
+
+        $liClassAttribute = ' class="'.implode(' ', $liClasses).'"';
+        $linkClassAttribute = ' class="'.implode(' ', $linkClasses).'"';
+        $linkAttributeString = empty($linkAttributes) ? '' : ' '.implode(' ', $linkAttributes);
+
+        echo '<li'.$liClassAttribute.'>';
+        echo '<a'.$linkClassAttribute.' href="'.htmlspecialchars($it['link']).'"'.$linkAttributeString.'>'.htmlspecialchars($it['label']).'</a>';
+
+        if ($hasChildren) {
             echo '<ul class="dropdown-menu">';
             renderMenu($it['children'], true);
             echo '</ul>';
-        } else {
-            echo '<li class="nav-item'.($isDropdown ? '' : '').'">';
-            echo '<a class="nav-link" href="'.htmlspecialchars($it['link']).'"'.(!empty($it['new_tab']) ? ' target="_blank"' : '').'>'.htmlspecialchars($it['label']).'</a>';
         }
+
         echo '</li>';
     }
 }
